b9422873bf5e0533acae1ca2bfdf60351f0f2f36
mc8mac <marcos.machado@tecnico.ulisboa.pt>
2023-11-15 19:11:45 +0000

    feat: added timeline and animations bug: image is not aligned yet

--- src/js/app.js
+++ src/js/app.js
@@ -1738,15 +1738,36 @@ particlesJS('particles-js',
 
 );
 
+(function () {
+  "use strict";
 
-const observer = new IntersectionObserver(entries => {
-  console.log(entries);
-  entries.forEach(entry => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("animate");
+  // define variables
+  var items = document.querySelectorAll(".timeline li");
+
+  // check if an element is in viewport
+  // http://stackoverflow.com/questions/123999/how-to-tell-if-a-dom-element-is-visible-in-the-current-viewport
+  function isElementInViewport(el) {
+    var rect = el.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  }
+
+  function callbackFunc() {
+    for (var i = 0; i < items.length; i++) {
+      if (isElementInViewport(items[i])) {
+        items[i].classList.add("in-view");
+      }
     }
-  })
-})
-observer.observe(document.querySelector(".container.left"));
-observer.observe(document.querySelector(".container.right"));
-observer.observe(document.querySelector(".content"));
+  }
+
+  // listen for events
+  window.addEventListener("load", callbackFunc);
+  window.addEventListener("resize", callbackFunc);
+  window.addEventListener("scroll", callbackFunc);
+})();
+
